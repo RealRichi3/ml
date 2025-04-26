1c1a94616f790fb262be6c9e307eedbfbbe38656
Richie <molunorichie@gmail.com>
2025-04-26 15:08:01 +0100

    generated overlapping clusters

--- ex_3/generate_and_plot_dataset.m
+++ ex_3/generate_and_plot_dataset.m
@@ -1,57 +1,26 @@
-% Example 3.1: K-Means Clustering
+% Filename: generate_and_plot_dataset.m
 
-% To generate the data
-Mean1=[-4; -1]; Std1=1.9;
-Mean2=[3; 4]; Std2 = 2.0;
-Samples=200;
-data1=Std1*randn(2, Samples) + repmat(Mean1, 1, Samples);
-data2=Std2*randn(2, Samples) + repmat(Mean2, 1, Samples);
+% Initialize random number generator for reproducibility
+rng(0);
 
-% To plot the two clusters
-plot(data1(1,:),data1(2,:),'or');
-hold on
-plot(data2(1,:),data2(2,:),'+b');
+% Step 1: Generate Dataset
+% Cluster 1
+mean1 = [-4, -1];
+std_dev1 = 0.75;
+cluster1 = std_dev1 * randn(1000, 2) + mean1;
 
-% To plot the data together
-figure
-plot([data1(1,:) data2(1,:)],[data1(2,:) data2(2,:)],'or');
+% Cluster 2
+mean2 = [3, 4];
+std_dev2 = 2.0;
+cluster2 = std_dev2 * randn(1000, 2) + mean2;
 
-datatogether=[data1 data2];
-[m,n]=size(datatogether);
-
-% To randomly select two centroids
-centroid1=datatogether(1:2,10);
-centroid2=datatogether(1:2,250);
-MaxIterations=20;
-
-% To divide the data into two clusters
-for i=1:1:MaxIterations
-    
-    cluster1=[];
-    cluster2=[];
-    centroid1old=centroid1;
-    
-    for j=1:1:n
-        distance1=(datatogether(:,j)-centroid1)'*(datatogether(:,j)-centroid1);
-        distance2=(datatogether(:,j)-centroid2)'*(datatogether(:,j)-centroid2);
-        if distance1 <= distance2
-           cluster1=[cluster1 datatogether(:,j)];
-        else
-           cluster2=[cluster2 datatogether(:,j)];
-        end
-    end
-    centroid1=(mean(cluster1'))';
-    centroid2=(mean(cluster2'))';
-    
-    % To stop the loop if the centroid is not changing
-    if centroid1 == centroid1old
-       break
-    end
-end
-
-% To plot the result of K-Means clustering
-figure
-plot(cluster1(1,:),cluster1(2,:),'+b');
-hold on
-plot(cluster2(1,:),cluster2(2,:),'or');
-hold on
+% Plot the clusters
+figure;
+scatter(cluster1(:,1), cluster1(:,2), 'r.');
+hold on;
+scatter(cluster2(:,1), cluster2(:,2), 'b.');
+title('2D Uncorrelated Dataset with Overlapping Clusters');
+xlabel('X-axis');
+ylabel('Y-axis');
+legend('Cluster 1', 'Cluster 2');
+hold off;
